8f26bba77b2bcebe86bfbed6a6f3bd24acd1be0c
mathilde samanos <mathilde.samanos@hei.yncrea.fr>
2019-02-11 14:32:12 +0100

    exercice blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercice-blog';
+
+  posts = [
+    {
+      title : 'Mon premier Post',
+      content : 'Et olim licet otiosae sint tribus pacataeque centuriae et nulla suffragiorum certamina set Pompiliani redierit securitas temporis, per omnes tamen quotquot sunt partes terrarum, ut domina suscipitur et regina et ubique patrum reverenda cum auctoritate canities populique Romani nomen circumspectum et verecundum.',
+      loveIts: 2
+    },
+    {
+      title : 'Mon second Post',
+      content : 'Et olim licet otiosae sint tribus pacataeque centuriae et nulla suffragiorum certamina set Pompiliani redierit securitas temporis, per omnes tamen quotquot sunt partes terrarum, ut domina suscipitur et regina et ubique patrum reverenda cum auctoritate canities populique Romani nomen circumspectum et verecundum.',
+      loveIts: 3
+    },
+    { title : 'Mon troisième Post',
+      content : 'Et olim licet otiosae sint tribus pacataeque centuriae et nulla suffragiorum certamina set Pompiliani redierit securitas temporis, per omnes tamen quotquot sunt partes terrarum, ut domina suscipitur et regina et ubique patrum reverenda cum auctoritate canities populique Romani nomen circumspectum et verecundum.',
+      loveIts: -2
+    }
+  ];
+
+  constructor() {
+
+  }
 }
